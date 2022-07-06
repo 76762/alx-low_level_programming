398625a5e630f79e81ea85ee953b7194d3610298
76762 <steinmbali@gmail.com>
2022-07-06 16:19:38 -0700

    0-putchar.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<time'h>
+#include <ctype.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+
+ */
+
+int main(void)
+
+{
+
+	int n = 'a';
+
+	while (l <= 'z')
+
+	{
+
+		putchar(l);
+
+		l += 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+_char _putchar(char c);
