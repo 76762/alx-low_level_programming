bb5df79ec2d5b56d138cc8f48be22de20b6e9407
76762 <steinmbali@gmail.com>
2022-07-06 17:16:11 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,23 +3,12 @@
  * print_alphabet - a function that prints the alphabet, in lowercase
  * Return: Always 0 (Succes)
  */
-
-
-
 void print_alphabet(void)
-
 {
-
-	int n;
-
-	for (n = 'a'; n <= 'z'; n++)
-
-	{
-
-		_putchar(n);
-
-	}
-
-	_putchar('\n');
-
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
 }
