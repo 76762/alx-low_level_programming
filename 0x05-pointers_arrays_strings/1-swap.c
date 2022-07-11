e639450eb33c5861eecfe4bb83a82085180a3682
76762 <steinmbali@gmail.com>
2022-07-11 04:39:31 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,3 +12,14 @@ aux = *a;
 *a = *b;
 *b = aux;
 }
+int main(void)
+}
+int a;
+int b;
+a = 98;
+b = 42;
+printf("a=%d, b=%d\n", a, b)
+swap_int(&a, &b);
+printf("a=%d, b=%d\n", a, b);
+return (0);
+}
